36f0a6c7ade1777dbc266a2eaed0c90148b4d27f
Paweł Stolecki <noigell@gmail.com>
2018-03-16 10:52:27 +0100

    poprawki zadanie-9-6

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-window.onload = function() {
-	console.log('The page is fully loaded.');
-
-	function checkOnclickEvent(zdarzenie) {
- 		console.log(zdarzenie);
-	}
-	var inputElem = document.getElementById('js-inputValue'),
-   		imageElem = document.getElementById('js-image'),
-   		buttonElem = document.getElementById('js-button'),
+function checkOnclickEvent(zdarzenie) {
+	console.log(zdarzenie);
+}
+var inputElem = document.getElementById('js-inputValue'),
+   	imageElem = document.getElementById('js-image'),
+   	buttonElem = document.getElementById('js-button'),
 //ZADANIE 9-6
-   		add = document.getElementById('addElem'),
-   		list = document.getElementById('list');
+   	add = document.getElementById('addElem'),
+   	list = document.getElementById('list');
    		
 	imageElem.addEventListener('click', function() {checkOnclickEvent('image was clicked')});
 	inputElem.addEventListener('click', function() {checkOnclickEvent('input was clicked')});
@@ -20,11 +17,6 @@ window.onload = function() {
 
 	add.addEventListener('click', function() {
 		var element = document.createElement('li');
-		var linr = document.getElementsByTagName('li');
-
-		element.innerHTML = 'item ' + linr.length;
-
+		element.innerHTML = 'item ' + list.children.length;
 		list.appendChild(element);
 	});
-
-}
